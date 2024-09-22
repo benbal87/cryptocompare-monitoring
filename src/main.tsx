78e80b1e5a6e8360e87e12aa6c4e6846b4a2e910
Benedek Balázs <benbal87@gmail.com>
2024-09-22 22:50:20 +0200

    :lipstick: Enable dark mode theme
    Added a new dark theme using MUI's createTheme in theme.ts. Updated index.css to use the dark color scheme and wrapped AppComponent in ThemeProvider in main.tsx to apply the theme globally.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
+import { ThemeProvider } from '@mui/material/styles'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import AppComponent from './app.component.tsx'
 import store from './redux/store'
 import './index.css'
+import darkTheme from './theme.ts'
 
 const rootElement = document.getElementById('root')!
 
@@ -11,7 +13,9 @@ const renderApp = () => {
   createRoot(rootElement).render(
     <StrictMode>
       <Provider store={store}>
-        <AppComponent />
+        <ThemeProvider theme={darkTheme}>
+          <AppComponent />
+        </ThemeProvider>
       </Provider>
     </StrictMode>
   )
---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { createTheme } from '@mui/material/styles'
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark' // Sets the theme mode to dark
+  }
+})
+
+export default darkTheme
